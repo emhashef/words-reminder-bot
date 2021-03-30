8fa4cce1419a53ab82c6782e1f6d437219e0e649
Mohammad Hosein Fallah <emhashef@gmail.com>
2021-03-31 02:11:44 +0430

    fix query bug

--- handlers/snooze_news_query.py
+++ handlers/snooze_news_query.py
@@ -4,13 +4,14 @@ from telegram.ext import CallbackQueryHandler
 
 @have_access
 def snooze_news_query(update,context,user):
-    hour = update.callback_query.split()[1]
+    query = update.callback_query
+    hour = query.split()[1]
     if not hour and hour.isnumeric():
         return
 
     user.ready_for_news_at = datetime.now() + timedelta(hours=int(hour))
     user.new_words = 0
     user.save()
-    update.callback_query.answer(f'snoozed for {hour} hour')
+    query.answer(text=f'snoozed for {hour} hour')
 
 handler = CallbackQueryHandler(snooze_news_query,pattern='snooze_news')
