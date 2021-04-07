b4d0ccb998417d74dd74f09a5bbd20b3978ae86a
Mohammad Hosein Fallah <emhashef@gmail.com>
2021-04-07 22:57:18 +0430

    add error handler

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import logging
 from utils.decorators import have_access
 from utils.image import generate_image
 from jobs.remind import remind
+from database import User
 import importlib
 import handlers
 import time
@@ -15,6 +16,33 @@ logging.basicConfig(level=logging.DEBUG,
 
 logger = logging.getLogger(__name__)
 
+def error_handler(update: object, context: CallbackContext) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = ''.join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f'An exception was raised while handling an update\n'
+        f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
+        '</pre>\n\n'
+        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+        f'<pre>{html.escape(tb_string)}</pre>'
+    )
+
+    user = User.where(username=config('username')).get_or_none()
+    
+    if user and user.chat_id:
+        # Finally, send the message
+        context.bot.send_message(chat_id=user.chat_id, text=message, parse_mode=ParseMode.HTML)
 
 @have_access
 def hello(update: Update, context: CallbackContext) -> None:
@@ -33,6 +61,7 @@ for handler_name in handlers.__all__:
     if handler:
         updater.dispatcher.add_handler(handler)
 
+updater.dispatcher.add_handler(error_handler)
 
 PORT = int(config('PORT', 5000))
 
