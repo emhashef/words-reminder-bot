8cbff8aa72705e6d2bbc41f79f013f6e3733d42d
Mohammad Hosein Fallah <45512247+emhashef@users.noreply.github.com>
2021-10-13 01:18:44 +0330

    fix dictionary

--- utils/dictionary.py
+++ utils/dictionary.py
@@ -13,11 +13,14 @@ def get_def_markdown(word: str):
 
     db_def = get_def_from_db(word.lower())
     if( db_def is None):
-        res = requests.get(f'https://abadis.ir/entofa/{word.lower()}')
-        soup = BeautifulSoup(res.content, 'html.parser')
-        translate = soup.select('.boxMain')[0].contents[3].replace(':','').strip()
-        Definition.create(word=word.lower(),definition=translate)
+        try:
 
+            res = requests.get(f'https://abadis.ir/entofa/{word.lower()}')
+            soup = BeautifulSoup(res.content, 'html.parser')
+            translate = soup.select('.boxMain')[0].contents[3].replace(':','').strip()
+            Definition.create(word=word.lower(),definition=translate)
+        except Exception:
+            translate = ''
     else:
         translate = db_def
     
